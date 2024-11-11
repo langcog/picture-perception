b03beb77801353560f92a16d1a11acd0101d42c2
vermaarn <arnavverma@hotmail.com>
2024-11-10 16:05:33 -0800

    adde image rotation

--- image_processing/extract_sketch_labels.py
+++ image_processing/extract_sketch_labels.py
@@ -1,22 +1,30 @@
 import easyocr
-import os 
+import os
+import re
 
 reader = easyocr.Reader(['en', 'sw'])  # 'en' == English; 'sw' == 'Swahili'
-
-base_dir = "../drawings_png"
-for file in sorted(os.listdir(base_dir)):
-    if not file.endswith(".png"):
-        continue
-    reader_results = reader.readtext(base_dir + "/" + file)
-
-    for (bbox, text, confidence) in reader_results:
-        print(f"File {file}, Text: {text}, Confidence: {confidence}")
-        
-    break
-    
-    
 def run_ocr(reader, filename):
-    reader_results = reader.readtext(base_dir + "/" + file)
-
+    reader_results = reader.readtext(base_dir + "/" + filename)
+    
+    labels = []
     for (bbox, text, confidence) in reader_results:
-        print(f"File {file}, Text: {text}, Confidence: {confidence}")
+        label = {"text": text, "confidence": confidence}
+        print(f"File {filename}, Text: {text}, Confidence: {confidence}")
+        
+
+
+def matche_first_page_pattern(s):
+    # match strings for first five pages "page_1" to "page_5"
+    pattern = r"^page_[1-5]$"
+    return bool(re.match(pattern, s))
+
+
+# base_dir = "../drawings_png"
+base_dir = "/Users/arnav/Desktop/picture-perception/drawings_png"
+for file in sorted(os.listdir(base_dir)):
+    if not file.endswith(".png") or matche_first_page_pattern(file):
+        continue
+
+    file_name = base_dir + "/" + file
+    run_ocr(reader, file_name)
+    
--- image_processing/split_pdfs.py
+++ image_processing/split_pdfs.py
@@ -1,29 +1,42 @@
 from pdf2image import convert_from_path
+from PIL import Image
 import os
+import subprocess
 
+## note: using defult python pdf2image lib doesn't work correctly since it keeps the extra white space within the pdf
+## while the CLI pdf2image acutally extracts the embedded image
 def split_pdfs(pdf_dir, img_dir):
     """Splits pdf files into separate png image files while making sure all images are horizontal"""
 
-    for file in os.listdir(pdf_dir):
+    for file in sorted(os.listdir(pdf_dir)):
         if not file.endswith('.pdf'):
             continue
-        if "Rescans" in file or "don_t" in file:
+        if file.find("don_t") != -1:
+            continue
+
+        print(pdf_dir + "/" + file)
+        pid = file.replace(".pdf", "")
+
+        file_path = os.path.join(pdf_dir, file)
+        subprocess.run(["pdfimages", "-png", file_path, img_dir + "/" + pid])
+
+
+def rotate_vertical_pdfs(img_dir):
+    """Rotate vertical images to horizontal images"""
+    for file in sorted(os.listdir(img_dir)):
+        if not file.endswith('.png'):
             continue
         
-        print(file)
-        pid = file.replace(".pdf", "")
-        images = convert_from_path(os.path.join(pdf_dir, file))
-        
-        for i, image in enumerate(images):
-            if image.height > image.width:
-                image = image.rotate(90, expand=True)
-            
-            file_name = os.path.join(img_dir, f"{pid}_page_{i+1}.png")
-            image.save(file_name, "PNG")
-            print(f"Saved page {i+1} as {file_name}")
-
+        file_path = os.path.join(img_dir, file)
+        image = Image.open(file_path) 
+  
+        if image.height > image.width:
+            print(file_path)
+            image = image.rotate(90, expand=True)
+            image.save(file_path, "PNG")
 
 if __name__ == "__main__":
     pdf_dir = "../Kisumu 2024 Drawings and Consents"
-    img_dir = "../drawings_png"
-    split_pdfs(pdf_dir, img_dir)
+    img_dir = "../drawing_images"
+    # split_pdfs(pdf_dir, img_dir)
+    rotate_vertical_pdfs(img_dir)
